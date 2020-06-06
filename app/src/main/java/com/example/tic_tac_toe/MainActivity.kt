5d0229d12a3f9a4cadbc2ff390e03030d5b143d3
Vishnu Basskar <vibass.vishnu@outlook.com>
2020-06-06 18:24:55 +0530

    Thks IS the final upfate on what I have learnt

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     fun buClick(view: View){
@@ -67,8 +68,6 @@ class MainActivity : AppCompatActivity() {
         if(player2.contains(1) && player2.contains(2) && player2.contains(3)){
             winner=2
         }
-
-
         // row 2
         if(player1.contains(4) && player1.contains(5) && player1.contains(6)){
             winner=1
@@ -76,10 +75,6 @@ class MainActivity : AppCompatActivity() {
         if(player2.contains(4) && player2.contains(5) && player2.contains(6)){
             winner=2
         }
-
-
-
-
         // row 3
         if(player1.contains(7) && player1.contains(8) && player1.contains(9)){
             winner=1
@@ -87,9 +82,6 @@ class MainActivity : AppCompatActivity() {
         if(player2.contains(7) && player2.contains(8) && player2.contains(9)){
             winner=2
         }
-
-
-
         // col 1
         if(player1.contains(1) && player1.contains(4) && player1.contains(7)){
             winner=1
@@ -97,9 +89,6 @@ class MainActivity : AppCompatActivity() {
         if(player2.contains(1) && player2.contains(4) && player2.contains(7)){
             winner=2
         }
-
-
-
         // col 2
         if(player1.contains(2) && player1.contains(5) && player1.contains(8)){
             winner=1
@@ -107,8 +96,6 @@ class MainActivity : AppCompatActivity() {
         if(player2.contains(2) && player2.contains(5) && player2.contains(8)){
             winner=2
         }
-
-
         // col 3
         if(player1.contains(3) && player1.contains(6) && player1.contains(9)){
             winner=1
@@ -116,7 +103,6 @@ class MainActivity : AppCompatActivity() {
         if(player2.contains(3) && player2.contains(6) && player2.contains(9)){
             winner=2
         }
-
         //diagonal
         if(player1.contains(1) && player1.contains(5) && player1.contains(9)){
             winner=1
@@ -124,14 +110,12 @@ class MainActivity : AppCompatActivity() {
         if(player2.contains(1) && player2.contains(5) && player2.contains(9)){
             winner=2
         }
-
         if(player1.contains(3) && player1.contains(5) && player1.contains(7)){
             winner=1
         }
         if(player2.contains(3) && player2.contains(5) && player2.contains(7)){
             winner=2
         }
-
         if( winner != -1){
 
             if (winner==1){
